72e69ef1edc41c492ed30e1b40e716183da5ef08
Bella Bernal <bella.bernal@atlasschool.com>
2025-05-19 13:02:15 -0500

    Stutor facing changes

--- src/main.c
+++ src/main.c
@@ -2,19 +2,5 @@
 #include "tip_calculator.h"  // make sure this header declares calculate_bill
 
 int main() {
-    float total_bill, tax_percentage, tip_percentage;
-
-    printf("Enter the total bill amount: ");
-    scanf("%f", &total_bill);
-
-    printf("Enter the tax percentage: ");
-    scanf("%f", &tax_percentage);
-
-    printf("Enter the tip percentage: ");
-    scanf("%f", &tip_percentage);
-
-    float total_with_tip = calculate_bill(total_bill, tax_percentage, tip_percentage);
-    printf("Total amount to be paid (including tax and tip): %.2f\n", total_with_tip);
-
-    return 0;
+   /* Your code here */
 }
